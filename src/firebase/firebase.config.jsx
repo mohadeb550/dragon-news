7ad2a21d4de6dab737d4431c53b0da07411d858a
mohadebDev <maxff00789@gmail.com>
2023-10-02 22:05:59 +0600

    files setup

---
+++ src/firebase/firebase.config.jsx
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3ay3HcCuVsNrX3CIRTabToc7TgnX73pQ",
+  authDomain: "dragon-news-auth-668b2.firebaseapp.com",
+  projectId: "dragon-news-auth-668b2",
+  storageBucket: "dragon-news-auth-668b2.appspot.com",
+  messagingSenderId: "421831855636",
+  appId: "1:421831855636:web:02409af4e7ad81537ba101"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
